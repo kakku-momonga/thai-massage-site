ad9e7cadbdc2a6ba7f9e2350beceac38f4f7e40d
kakku-momonga <57647421+kakku-momonga@users.noreply.github.com>
2026-01-23 17:17:44 +0900

    どのホストからでも表示できるようにbaseを修正した

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => ({
   base: mode === 'production'
-    ? '/thai-massage-site/'
+    ? './'
     : '/',
   plugins: [react()],
 }))
